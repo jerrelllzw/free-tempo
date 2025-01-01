c12e1087c5b9af38ae1a851d335e088c628916e8
jerrelllzw <lowjerrell47@gmail.com>
2025-01-01 15:54:29 +0800

    Remove comment

--- bot_commands.py
+++ bot_commands.py
@@ -16,7 +16,7 @@ ydl_opts = {
 
 ffmpeg_opts = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn'  # Only stream audio (no video)
+    'options': '-vn'
 }
 
 
